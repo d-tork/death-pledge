f1fc850bc4c90e9d348b1cfa2296bfb744a088ad
d-tork <dtorkelson@gmail.com>
2019-07-02 20:15:14 -0400

    Add scraping moduleˆ

---
+++ scrape.py
@@ -0,0 +1,21 @@
+"""Scrape listings for data I would normally input myself."""
+
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+browser = webdriver.Firefox()
+url = r"https://daniellebiegner.realscout.com/homesearch/listings/p-10217-rolling-green-way-fort-washington-20744-brightmls-158"
+browser.get(url)
+html_source = browser.page_source
+
+soup = BeautifulSoup(html_source, features='html.parser')
+tags = soup.html.find_all('a', 'show-more-details')
+browser.find_element_by_class_name('show-more-details').click()
+
+home_dict = {}
+home_dict['sale_price'] = soup.find('p', 'price').contents[0]
+home_dict['list_price'] = soup.find('p', 'price-listed').contents[0]
+home_dict['status'] = soup.find('p', 'status').contents[0]
+
+for k, v in home_dict.items():
+    print('{}: {}'.format(k, v))

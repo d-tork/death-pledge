45a1484e49a499bc802ddc7e75ec5f7152fac863
d-tork <dtorkelson@gmail.com>
2019-08-14 22:42:30 -0400

    Convert HOA currency to int; close #14

--- Code/clean.py
+++ Code/clean.py
@@ -1,8 +1,10 @@
 """Clean a dictionary of listing details."""
 
 import time
-import Code
-from Code import json_to_pandas
+import glob
+import os
+from Code import PROJ_PATH
+from Code import json_to_pandas, scrape2
 
 
 def convert_currency_to_int(s):
@@ -12,8 +14,6 @@ def convert_currency_to_int(s):
 def parse_string_float(s):
     if 'bath' in s.split()[1].lower():
         return float(s.split()[0])
-    else:
-        return int(s.split()[0])
 
 
 def parse_date(s):
@@ -24,6 +24,7 @@ currency_list = [
     ('info', 'list_price'),
     ('info', 'sale_price'),
     ('basic_info', 'Price Per SQFT'),
+    ('basic_info', 'HOA Fee'),
     ('Building Information', 'Price Per SQFT'),
     ('Association / Location / Schools', 'Condo/Coop Fee'),
 ]
@@ -42,9 +43,15 @@ def main(dic):
     for k1, v1 in dic.items():
         for k2, v2 in v1.items():
             if k2 in [x[1] for x in currency_list]:
-                v1[k2] = convert_currency_to_int(v2)
+                try:
+                    v1[k2] = convert_currency_to_int(v2)
+                except AttributeError:
+                    pass
             if k2 in [x[1] for x in int_parse_list]:
-                v1[k2] = parse_string_float(v2)
+                try:
+                    v1[k2] = parse_string_float(v2)
+                except AttributeError:
+                    pass
             #if k2 in [x[1] for x in date_list]:
             #    v1[k2] = parse_date(v2)
             try:
@@ -54,6 +61,10 @@ def main(dic):
 
 
 if __name__ == '__main__':
-    dic1 = json_to_pandas.sample_main()
-    main(dic1)
+    #dic1 = json_to_pandas.sample_main()
+    #main(dic1)
 
+    for f in glob.glob(os.path.join(PROJ_PATH, 'Data', 'Processed', 'saved_listings', '*.json')):
+        house = json_to_pandas.read_list_from_json(f)[0]
+        main(house)
+        _ = scrape2.add_dict_to_file(house)

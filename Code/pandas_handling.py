733ba61dda5b7758ee01680a831a5b0fb914ecf0
The Tern <dtorkelson@gmail.com>
2019-12-24 16:27:06 -0800

    Upload raw data to google sheets

--- Code/pandas_handling.py
+++ Code/pandas_handling.py
@@ -5,12 +5,8 @@ import Code
 from Code import score2, json_handling
 
 
-def merge_data_and_scores(slim=True):
-    """Read in data from JSONs and scorecards, then merge as dataframe.
-
-    Args:
-        slim (bool): If True, then only merge with TOTAL_SCORE.
-    """
+def merge_data_and_scores():
+    """Read in data from JSONs and scorecards, then merge as dataframe. """
     sc_path = os.path.join(Code.PROJ_PATH, 'Data', 'Processed', 'scorecards.json')
     scores = json_handling.read_dicts_from_json(sc_path)
     df_scores = score2.score_dict_list_to_dataframe(scores)
@@ -31,15 +27,11 @@ def merge_data_and_scores(slim=True):
     df_data = json_handling.all_files_to_dataframe(Code.LISTINGS_GLOB).T
 
     # Merge on indices
-    if slim:
-        df_scores = df_scores[('scores', 'TOTAL_SCORE')]
-    merged = pd.merge(df_data, df_scores,
+    merged = pd.merge(df_data, df_scores, how='left',
                       left_index=True, right_index=True)
     # Drop the top level of the column multiindex (Excel tables don't like it)
     merged = merged.droplevel(level=0, axis=1)
-    # Set column headers
-    if slim:
-        merged = clean_dataframe_columns(merged)
+    df_scores = df_scores.droplevel(level=0, axis=1)
     # Name the index
     merged.index.name = 'MLS Number'
     # Sort by total score
@@ -51,7 +43,7 @@ def merge_data_and_scores(slim=True):
     return merged, df_scores
 
 
-def clean_dataframe_columns(df):
+def master_list_columns(df):
     col_list = [
         'TOTAL_SCORE',
         'URL',

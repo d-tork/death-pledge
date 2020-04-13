55a3d591f500a718234a42b8393b24ac3ac84750
The Spine <dtorkelson@gmail.com>
2020-04-13 16:09:19 -0400

    feat (scrape): specify geckodriver based on platform

--- Code/__init__.py
+++ Code/__init__.py
@@ -1,10 +1,15 @@
 from os import path, makedirs
+from sys import platform
 
 PROJ_PATH = path.normpath(path.join(path.dirname(path.realpath(__file__)), '..'))
-GECKODRIVER_PATH = path.join(PROJ_PATH, 'Code', 'Drivers', 'geckodriver')
 LISTINGS_DIR = path.join(PROJ_PATH, 'Data', 'Processed', 'saved_listings')
 LISTINGS_GLOB = path.join(PROJ_PATH, 'Data', 'Processed', 'saved_listings', '*.json')
 SCORECARD_PATH = path.join(PROJ_PATH, 'Data', 'scorecard.json')
+
+if platform == 'linux':
+    GECKODRIVER_PATH = path.join(PROJ_PATH, 'Code', 'Drivers', 'geckodriver_linux')
+else:
+    GECKODRIVER_PATH = path.join(PROJ_PATH, 'Code', 'Drivers', 'geckodriver')
 
 makedirs(LISTINGS_DIR, exist_ok=True)
 

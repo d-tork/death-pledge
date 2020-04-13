5252251f2732751ea8d18113afb4c181fb9cf92b
The Spine <dtorkelson@gmail.com>
2020-04-13 15:36:58 -0400

    fix: create data folder for new installations

--- Code/__init__.py
+++ Code/__init__.py
@@ -1,7 +1,10 @@
-from os import path
+from os import path, makedirs
 
 PROJ_PATH = path.normpath(path.join(path.dirname(path.realpath(__file__)), '..'))
 GECKODRIVER_PATH = path.join(PROJ_PATH, 'Code', 'Drivers', 'geckodriver')
 LISTINGS_DIR = path.join(PROJ_PATH, 'Data', 'Processed', 'saved_listings')
 LISTINGS_GLOB = path.join(PROJ_PATH, 'Data', 'Processed', 'saved_listings', '*.json')
 SCORECARD_PATH = path.join(PROJ_PATH, 'Data', 'scorecard.json')
+
+makedirs(LISTINGS_DIR, exist_ok=True)
+

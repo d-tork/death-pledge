5d8c0d0f0934c6ed6c887b603598703f54a0d0d8
The-Spine <dtorkelson@gmail.com>
2020-10-30 12:28:06 -0400

    fix: drop null rows in refresh of google sheets
    A design doc is being picked up by the view, and as it is not a home
    listing, it has null fields which can't be parsed as JSON when passed
    to Google sheets.

--- deathpledge/api_calls/google_sheets.py
+++ deathpledge/api_calls/google_sheets.py
@@ -155,6 +155,7 @@ def refresh_url_sheet(google_creds):
         columns=['added_date', 'status', 'url', 'mls_number', 'full_address', 'docid']
     )
     url_df['added_date'] = pd.to_datetime(url_df['added_date']).dt.strftime('%m/%d/%Y')
+    url_df.dropna(subset=['added_date'], inplace=True)
     url_df = url_df.set_index('added_date')
     url_list = prep_dataframe_to_update_google(url_df)
 

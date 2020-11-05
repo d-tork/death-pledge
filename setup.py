cfef816f25e88c6f9e40a65edcc6e1010fa25b95
The-Spine <dtorkelson@gmail.com>
2020-11-04 22:07:31 -0500

    chore: update setup and requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,30 @@ from setuptools import setup, find_packages
 setup(
     name='deathpledge',
     version='20.1',
+    author='Daniel Torkelson',
     packages=find_packages(include=['deathpledge', 'deathpledge.*']),
+    install_requires=[
+        'beautifulsoup4',
+        'cloudant',
+        'fake-useragent',
+        'google-api-core',
+        'google-api-python-client',
+        'google-auth',
+        'google-auth-httplib2',
+        'google-auth-oauthlib',
+        'googleapis-common-protos',
+        'gspread',
+        'httplib2',
+        'numpy',
+        'pandas',
+        'probableparsing',
+        'PyYAML',
+        'rsa',
+        'selenium',
+        'sqlparse',
+        'urllib3',
+        'usaddress'
+    ],
     entry_points={
         'console_scripts': [
             'main=deathpledge.main:main',

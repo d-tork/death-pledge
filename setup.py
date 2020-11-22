7be67c5991ae5cce176c5d43d0240729313fc90a
The Tern <dtorkelson@gmail.com>
2020-11-22 11:47:04 -0500

    move package installation reqs to setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,27 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='deathpledge',
-    version='20.1',
+    name='death-pledge',
+    version='2020.11.22',
+    author='Daniel Torkelson',
     packages=find_packages(include=['deathpledge', 'deathpledge.*']),
     entry_points={
         'console_scripts': [
             'main=deathpledge.main:main',
             'single=deathpledge.sample:main'
         ]
-    }
+    },
+    install_requires=[
+        'beautifulsoup4',
+        'cloudant',
+        'Django',
+        'fake-useragent',
+        'google-api-python-client',
+        'google-auth-httplib2',
+        'google-auth-oauthlib',
+        'pandas',
+        'PyYAML',
+        'selenium',
+        'usaddress'
+        ]
 )

cdfb4d32d31601a73cfdbb401d46d2c8d3bca763
The-Spine <dtorkelson@gmail.com>
2020-11-25 16:55:14 -0500

    feat: make logging setup/config look like work

---
+++ deathpledge/logs/log_setup.py
@@ -0,0 +1,52 @@
+"""
+My newest headache.
+
+https://stackoverflow.com/questions/45287578/yet-another-python-logging-setup
+"""
+import os
+import yaml
+import logging
+import logging.config
+
+LOGDIR = os.path.normpath(os.path.dirname(os.path.realpath(__file__)))
+LOGCONFIG_FILE = os.path.join(LOGDIR, '../../config/logging.yaml')
+
+
+def dp_infologger():
+    """Log INFO messages and higher regarding the deathpledge package."""
+    logfile = os.path.join(LOGDIR, 'dp_info.log')
+    logger = logging.handlers.RotatingFileHandler(
+        filename=logfile,
+        maxBytes=3*1024**2,  # 3MB
+        backupCount=1
+    )
+    return logger
+
+
+def dp_debugger():
+    """Log DEBUG messages and higher regarding the deathpledge package."""
+    logfile = os.path.join(LOGDIR, 'dp_debug.log')
+    logger = logging.handlers.RotatingFileHandler(
+        filename=logfile,
+        maxBytes=3*1024**2,  # 3MB
+        backupCount=1
+    )
+    return logger
+
+
+def setup_logging(
+        config_path=LOGCONFIG_FILE,
+        default_level=logging.INFO,
+        env_key='LOG_CFG'
+):
+    path = config_path
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = yaml.safe_load(f.read())
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
+
